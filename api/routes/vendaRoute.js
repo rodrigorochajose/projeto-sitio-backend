5af27101c23f6381aa5475c07b78ef6cabac4223
Rodrigo Rocha José <rodrigorochajose@hotmail.com>
2022-11-18 16:30:56 -0300

    Conclusão

--- api/routes/vendaRoute.js
+++ api/routes/vendaRoute.js
@@ -4,8 +4,8 @@ const vendaController = require("../controllers/vendaController");
 const router = Router();
 
 router
-  .get("/vendas", vendaController.buscarVendas)
+  .get("/vendas", vendaController.buscarTodasVendas)
   .get("/venda/:id", vendaController.buscarVenda)
-  .post("/venda/nova", vendaController.geraNovaVenda)
+  .post("/venda/nova", vendaController.criaVenda)
   .put("/venda/atualizar/:id", vendaController.atualizarVenda);
 module.exports = router;
