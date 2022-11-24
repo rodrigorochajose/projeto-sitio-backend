e05910780012e8070457050b604753b8a3385d08
Rodrigo Rocha José <rodrigorochajose@hotmail.com>
2022-11-23 23:51:28 -0300

    Integração Front

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,20 @@ const clientes = require("./clienteRoute");
 const produtos = require("./produtoRoute");
 const vendas = require("./vendaRoute");
 const entregas = require("./entregaRoute");
+const cors = require("cors");
 
 module.exports = (app) => {
+  app.use("/*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
+    app.use(cors());
+    next();
+  });
+
   app.use(bodyParser.json(), usuarios, clientes, produtos, vendas, entregas);
 };
